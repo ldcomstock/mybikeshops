e870a780db151ba757b16a9357e9fdcba533cd59
Lynn Comstock <lynndcomstock@gmail.com>
2021-02-18 02:07:26 -0600

    fix issue with nav controller + FragmentContainerView

--- app/src/main/java/com/example/mybikeshops/MainActivity.kt
+++ app/src/main/java/com/example/mybikeshops/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.mybikeshops
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavHostController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = this.findNavController(R.id.navHostFragment)
-        NavigationUI.setupActionBarWithNavController(this,navController)
+
+        // Note: this wasn't a problem until I followed the lint suggestion of using FragmentContainerView
+        // instead of the <fragment> tag
+        // TODO: see issue: https://issuetracker.google.com/issues/142847973
+        // once resolved, the following code can be re-added
+        // val navController = this.findNavController(R.id.navHostFragment)
+        
+        // until then, we must instead do:
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
